a1a31b19ef212ca0037524e74f3a6a4abb3d36c9
Necro- <necrotto@gmail.com>
2024-01-18 17:20:11 +0200

    Part 1.14 (final task of Part 1).  Implemented showing the anecdote with the most votes and moved the anecdotes to their own file

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,24 +1,10 @@
 import { useState } from 'react'
-
+import Anecdotes from './components/Anecdotes'
 const App = () => {
-    const anecdotes = [
-        'If it hurts, do it more often.',
-        'Adding manpower to a late software project makes it later!',
-        'The first 90 percent of the code accounts for the first 90 percent of the development time...' +
-        'The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-        'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-        'Premature optimization is the root of all evil.',
-        'Debugging is twice as hard as writing the code in the first place. Therefore, ' +
-        'if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-        'Programming without an extremely heavy use of console.log is same as if a doctor would refuse ' +
-        'to use x-rays or blood tests when diagnosing patients.',
-        'The only way to go fast, is to go well.'
-    ]
-    
     const [selected, setSelected] = useState(0)
-    const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+    const [votes, setVotes] = useState(new Array(Anecdotes.length).fill(0))
     const RandomAnecdote = () => {
-        let randomNumber = Math.floor(Math.random() * Math.floor(anecdotes.length))
+        let randomNumber = Math.floor(Math.random() * Math.floor(Anecdotes.length))
         setSelected(randomNumber)
     }
     const VoteForAnecdote = () => {
@@ -26,17 +12,25 @@ const App = () => {
         copy[selected] += 1
         setVotes(copy)
     }
+    
+    const highestVote = Math.max(...votes)
+    const WinningAnecdote = Anecdotes[votes.indexOf(highestVote)]
     return (
         <div>
-            <p>{anecdotes[selected]}</p>
+            <Header text='Anecdote of the day'/>
+            <Anecdote anecdote={Anecdotes[selected]}/>
             <Votes votes={votes[selected]}/>
             <Button handleClick={VoteForAnecdote} text='Vote'/>
             <Button handleClick={RandomAnecdote} text='Next Anecdote'/>
+            <Header text='Anecdote with the most votes'/>
+            <Anecdote anecdote={WinningAnecdote}/>
+            <Votes votes={highestVote}/>
         </div>
     )
 }
-
-const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Header = ({text}) => <h1>{text}</h1>
+const Anecdote = ({anecdote}) => <p>{anecdote}</p>
 const Votes = ({votes}) =>  <p>has {votes} votes</p>
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
 export default App
