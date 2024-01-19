ff62c56c01dd2e4335e58e8edb334a94eea604dd
Necro- <necrotto@gmail.com>
2024-01-19 13:43:39 +0200

    Part 2.1-2.3. Modified course info so that map and reduce are used to determine the amount of total exercises.

---
+++ part2/courseinfo/src/components/Content.jsx
@@ -0,0 +1,10 @@
+﻿import Part from "./Part.jsx";
+const Content = (props) => {
+    const parts = props.parts.map((part, index) =>  <Part key={index} part={part.name} exercises={part.exercises} />)
+    return (
+        <div>
+            {parts}
+        </div>
+    )
+}
+export default Content
---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+﻿const Total = ({exercises}) => {
+    const total = exercises.reduce((sum, exerciseCount) => sum + exerciseCount)
+    return (
+        <p>
+            <b>Total of {total} exercises</b>
+        </p>
+    )
+}
+export default Total;
