9c1b171818df5470265c6ed92bc837569941a1dc
Necro- <necrotto@gmail.com>
2024-02-02 17:22:00 +0200

    Part 3.3-3.6. added delete, post functionality as well as error messages if you do something you shouldn't be able to (add an existing name etc.)

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,12 +1,14 @@
 ﻿const express = require('express')
 const app = express()
 
+app.use(express.json())
+
     let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
+        {
+            "id": 1,
+            "name": "Arto Hellas",
+            "number": "040-123456"
+        },
         {
             "id": 2,
             "name": "Ada Lovelace",
@@ -20,20 +22,59 @@ const app = express()
         {
             "id": 4,
             "name": "Mary Poppendieck",
-            "number": "39-23-6423122"
+            "number": "34-23-6423122"
         }
     ]
 
-app.get(`/`, (request, response) => {
+app.get('/', (request, response) => {
     response.send(`<h1>Hello world!</h1>`)
 })
 
-app.get(`/api/persons`, (request, response) => {
+app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-app.get(`/info`, (request, response) => {
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).send("<h1>ERROR(404):</h1> </br>The id you have inputted does not exist.").end()
+    }
+})
+
+app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${persons.length} people</br> ${Date()}</p>`)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
+    return (Math.floor(Math.random() * (99999 - maxId) + maxId))
+}
+
+app.post('/api/persons/', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({ error: 'name or number missing'})
+    }
+    if (persons.find(person => person.name === body.name )) {
+        return response.status(400).json({ error: 'name already exists in phonebook.'})
+    }
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 const PORT = 3001
