44b621f1047ec424c4c08b49d36f2c6bf4b97967
Necro- <necrotto@gmail.com>
2024-02-05 14:54:59 +0200

    Task 3.8: modified morgan to include content in POST

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -3,7 +3,11 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
+
+morgan.token('content', (request) => 
+    request.method === 'POST' && request.body.name ? 
+        JSON.stringify(request.body) : null)
 
     let persons = [
         {
