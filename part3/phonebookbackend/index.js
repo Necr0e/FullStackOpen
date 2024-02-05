2533b1cb1d3f181ec72e9c60acaeea8e907d69b0
Necro- <necrotto@gmail.com>
2024-02-05 19:35:21 +0200

    Tasks 3.13-3.15. Added code so frontend and backend work with mongoDB, get, post and delete work.

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -1,6 +1,8 @@
-﻿const express = require('express')
+﻿require('dotenv').config()
+const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 app.use(express.json())
@@ -8,85 +10,60 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-morgan.token('content', (request) => 
-    request.method === 'POST' && request.body.name ? 
+morgan.token('content', (request) =>
+    request.method === 'POST' && request.body.name ?
         JSON.stringify(request.body) : null)
 
-    let persons = [
-        {
-            "id": 1,
-            "name": "Arto Hellas",
-            "number": "040-123456"
-        },
-        {
-            "id": 2,
-            "name": "Ada Lovelace",
-            "number": "39-44-5323523"
-        },
-        {
-            "id": 3,
-            "name": "Dan Abramov",
-            "number": "12-43-234345"
-        },
-        {
-            "id": 4,
-            "name": "Mary Poppendieck",
-            "number": "34-23-6423122"
-        }
-    ]
-
-app.get('/', (request, response) => {
-    response.send(`<h1>Hello world!</h1>`)
-})
-
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person
+        .find({})
+        .then(persons => {
+            response.json(persons)
+    })
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).send("<h1>ERROR(404):</h1> </br>The id you have inputted does not exist.").end()
-    }
+app.get('/api/persons/:id', (request, response) => {
+    Person
+        .findById(request.params.id)
+        .then(person => {
+            response.json(person)
+        })
 })
 
-app.get('/info', (request, response, next) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</br> ${Date()}</p>`)
+app.get('/info', (request, response) => {
+    Person
+        .find({})
+        .then(persons => {
+            response.send(
+                `<div> <p>Phonebook has info for ${persons.length} people</p> <div><p>${Date()}</p></div>`
+            )
+    })
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
-    const id = Number(request.params.id)
-    persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response) => {
+    Person
+        .findByIdAndDelete(request.params.id)
+        .then(() => response.status(204).end())
 })
 
-const generateId = () => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map(person => person.id)) : 0
-    return (Math.floor(Math.random() * (99999 - maxId) + maxId))
-}
+app.post('/api/persons/', (request, response) => {
+    const newPerson = request.body
 
-app.post('/api/persons/', (request, response, next) => {
-    const body = request.body
-
-    if (!body.name || !body.number) {
+    if (!newPerson.name || !newPerson.number) {
         return response.status(400).json({ error: 'name or number missing'})
     }
-    if (persons.find(person => person.name === body.name )) {
-        return response.status(400).json({ error: 'name already exists in phonebook.'})
-    }
-    const person = {
-        id: generateId(),
-        name: body.name,
-        number: body.number,
-    }
-    persons = persons.concat(person)
-    response.json(person)
+    
+    const person = new Person ({
+        name: newPerson.name,
+        number: newPerson.number,
+    })
+    person.save()
+        .then(savedPerson => {
+            response.json(savedPerson)
+    })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is listening to port ${PORT}`)
 })
