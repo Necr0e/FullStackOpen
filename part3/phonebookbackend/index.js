8917b8413f503ccf155735ce067dbaf32c6542af
Necro- <necrotto@gmail.com>
2024-02-07 14:13:49 +0200

    Task 3.16-3.18. added errorhanding as a middleware, made sure updating user info works and that routes correctly point to database.

--- part3/phonebookbackend/index.js
+++ part3/phonebookbackend/index.js
@@ -21,15 +21,6 @@ app.get('/api/persons', (request, response) => {
             response.json(persons)
     })
 })
-
-app.get('/api/persons/:id', (request, response) => {
-    Person
-        .findById(request.params.id)
-        .then(person => {
-            response.json(person)
-        })
-})
-
 app.get('/info', (request, response) => {
     Person
         .find({})
@@ -37,16 +28,30 @@ app.get('/info', (request, response) => {
             response.send(
                 `<div> <p>Phonebook has info for ${persons.length} people</p> <div><p>${Date()}</p></div>`
             )
-    })
+        })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person
+        .findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        }).catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
     Person
         .findByIdAndDelete(request.params.id)
-        .then(() => response.status(204).end())
+        .then(() => { 
+            response.status(204).end()})
+        .catch(error => next(error))
 })
 
-app.post('/api/persons/', (request, response) => {
+app.post('/api/persons/', (request, response, next) => {
     const newPerson = request.body
 
     if (!newPerson.name || !newPerson.number) {
@@ -61,7 +66,37 @@ app.post('/api/persons/', (request, response) => {
         .then(savedPerson => {
             response.json(savedPerson)
     })
+        .catch(error => next(error))
 })
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+    Person.findByIdAndUpdate(request.params.id, person, {new: true}).then(updatedPerson => {
+        response.json(updatedPerson)
+    }).catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformed id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).send({ error: error.message })
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
