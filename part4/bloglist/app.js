15664357af7ae8f694258b9a6a7e73d93b4c0967
Necro- <necrotto@gmail.com>
2024-02-08 15:05:48 +0200

    Task 4.2 - refactored into sub-modules

---
+++ part4/bloglist/app.js
@@ -0,0 +1,26 @@
+﻿const blogsRouter = require('./controllers/blogs');
+const config = require('./utils/config');
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+
+const app = express();
+
+logger.info('Connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI).then(() => {
+    logger.info('Connected to MongoDB')
+}).catch((error) => {
+    logger.error('Error connecting to MongoDB:', error.message)
+})
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', blogsRouter);
+
+app.use(middleware.unknownEndpoint)
+
+module.exports = app;
---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,19 @@
+﻿const Blog = require('../models/blog');
+const express = require('express');
+const router = express.Router();
+
+router.get('/api/blogs', (_request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs);
+    });
+});
+
+router.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body);
+
+    blog.save().then((result) => {
+        response.status(201).json(result);
+    });
+});
+
+module.exports = router;
