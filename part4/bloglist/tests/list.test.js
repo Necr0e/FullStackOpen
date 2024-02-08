c7b7af55113b1b3f71b03d086377d38821a4d784
Necro- <necrotto@gmail.com>
2024-02-08 22:55:48 +0200

    Task 4.6-4.7 - Tests for checking number of blogs by the most popular author as well as the most likes an Author has.

--- part4/bloglist/tests/list.test.js
+++ part4/bloglist/tests/list.test.js
@@ -1,4 +1,6 @@
 ﻿const testHelper = require('../utils/test_Helper')
+const blogs = require('../utils/test_Data')
+const blog = blogs.blogList
 
 test('dummy returns one', () => {
     const blogs = []
@@ -7,88 +9,66 @@ test('dummy returns one', () => {
     expect(result).toBe(1)
 })
 
-describe('Total likes', () => {
+describe('test likes', () => {
     test('returns 0 for empty list', () => {
-        const blogs = []
-        const result = testHelper.totalLikes(blogs)
+        const emptyBlog = []
+        const result = testHelper.totalLikes(emptyBlog)
         expect(result).toBe(0)
     })
 
     test('When list has only one blog', () => {
-        const blogs = [ { name: 'test', likes: 4}]
-        const result = testHelper.totalLikes(blogs)
+        const oneBlog = [ { name: 'test', likes: 4}]
+        const result = testHelper.totalLikes(oneBlog)
         expect(result).toBe(4)
         })
     test('Larger list should return sum of likes in the list', () => {
-        const blogs = [{ name: 'test', likes: 5}, {name: 'test2', likes: 10}, {name: 'test3', likes: 50}]
-        const result = testHelper.totalLikes(blogs)
-        expect(result).toBe(65)
+        const result = testHelper.totalLikes(blog)
+        expect(result).toBe(36)
     })
 })
 
-describe('Favorite blog', () => {
+describe('Favorite Blog', () => {
     test('returns empty object if array is empty ', () => {
-        const blogs = []
-        const result = testHelper.FavoriteBlog(blogs)
+        const emptyBlog = []
+        const result = testHelper.FavoriteBlog(emptyBlog)
         expect(result).toStrictEqual({})
     })
     test('Returns blog with only one in list', () => {
-        const blogs = [{ title: 'test title', likes: 5}]
-        const result = testHelper.FavoriteBlog(blogs)
-        expect(result).toBe(blogs[0])
+        const oneBlog = [ { title: 'test name', author: 'Test Dummy', likes: 4}]
+        const result = testHelper.FavoriteBlog(oneBlog)
+        expect(result).toEqual({
+            title: 'test name',
+            author: 'Test Dummy',
+            likes: 4
+        })
     })
     test('Blog with most likes', () => {
-        const blogs = [
-            {
-                _id: "5a422a851b54a676234d17f7",
-                title: "React patterns",
-                author: "Michael Chan",
-                url: "https://reactpatterns.com/",
-                likes: 7,
-                __v: 0
-            },
-            {
-                _id: "5a422aa71b54a676234d17f8",
-                title: "Go To Statement Considered Harmful",
-                author: "Edsger W. Dijkstra",
-                url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-                likes: 5,
-                __v: 0
-            },
-            {
-                _id: "5a422b3a1b54a676234d17f9",
-                title: "Canonical string reduction",
-                author: "Edsger W. Dijkstra",
-                url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
-                likes: 12,
-                __v: 0
-            },
-            {
-                _id: "5a422b891b54a676234d17fa",
-                title: "First class tests",
-                author: "Robert C. Martin",
-                url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
-                likes: 10,
-                __v: 0
-            },
-            {
-                _id: "5a422ba71b54a676234d17fb",
-                title: "TDD harms architecture",
-                author: "Robert C. Martin",
-                url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-                likes: 0,
-                __v: 0
-            },
-            {
-                _id: "5a422bc61b54a676234d17fc",
-                title: "Type wars",
-                author: "Robert C. Martin",
-                url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
-                likes: 2,
-                __v: 0
-            }
-        ]
-        const result = testHelper.FavoriteBlog(blogs)
-        expect(result).toBe(blogs[2])
+
+        const result = testHelper.FavoriteBlog(blog)
+        expect(result).toEqual({
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            likes: 12
+        })
+    })
+})
+
+describe('Most Blogs', () => {
+    test('Most Blogs should be 3', () => {
+        const result = testHelper.MostBlogs(blog)
+        expect(result).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+    })
+})
+
+describe('Most Likes', () => {
+    test('Most likes should be 17', () => {
+        const result = testHelper.MostLikes(blog)
+        expect(result).toEqual({
+            author: 'Edsger W. Dijkstra',
+            likes: 17
+        })
     })
 })
--- part4/bloglist/utils/test_Helper.js
+++ part4/bloglist/utils/test_Helper.js
@@ -1,4 +1,5 @@
-﻿const dummy = (blogs) => {
+﻿const lodash = require('lodash')
+const dummy = (blogs) => {
     return 1
 }
 
@@ -9,9 +10,47 @@ const totalLikes = (blogs) => {
 
 const FavoriteBlog = (blogs) => {
     if (blogs.length === 0) return {}
-    const highestLikes = Math.max(...blogs.map(blog => blog.likes))
-    const indexToReturn = blogs.findIndex(blog => blog.likes === highestLikes)
-    return blogs[indexToReturn]
+    const blogLikes = blogs.map(blog => blog.likes)
+    const largestIndex = blogLikes.indexOf(Math.max(...blogLikes))
+    const largestInfo = blogs[largestIndex]
+
+    return {
+        title: largestInfo.title,
+        author: largestInfo.author,
+        likes: largestInfo.likes
+    }
 }
 
-module.exports = { dummy, totalLikes, FavoriteBlog}
+const MostBlogs = (blogs) => {
+    if (blogs.length === 0) return {}
+    let authors = blogs.map(blog => blog.author)
+    authors = [...new Set(authors)]
+
+    let published = new Array(authors.length).fill(0)
+    blogs.map(blog => published[authors.indexOf(blog.author)] += 1)
+
+    let index = published.indexOf(Math.max(...published))
+
+    return {
+        author: authors[index],
+        blogs: published[index]
+    }
+}
+
+const MostLikes = (blogs) => {
+    if (blogs.length === 0) return {}
+    let authors = blogs.map(blog => blog.author)
+    authors = [...new Set(authors)]
+
+    let totalLikes = new Array(authors.length).fill(0)
+    blogs.map(blog => totalLikes[authors.indexOf(blog.author)] += blog.likes)
+
+    let index = totalLikes.indexOf(Math.max(...totalLikes))
+
+    return {
+        author: authors[index],
+        likes: totalLikes[index]
+    }
+}
+
+module.exports = { dummy, totalLikes, FavoriteBlog, MostBlogs, MostLikes}
