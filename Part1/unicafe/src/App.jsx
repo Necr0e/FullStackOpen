e9920ff00d42a27d71b360bae884aafcb700ae1e
Necro- <necrotto@gmail.com>
2024-06-26 14:55:08 +0300

    Unicafe 1.8-1.11. Put stastics in a table, moved header, statistics and buttons to their own component files

--- Part1/unicafe/src/App.jsx
+++ Part1/unicafe/src/App.jsx
@@ -1,27 +1,24 @@
 import { useState } from 'react'
+import Header from './components/Header.jsx'
+import Button from './components/Button.jsx'
+import Statistics from './components/Statistics.jsx'
 
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
 
-    const total = good + neutral + bad
+    const handleGoodClick = () => setGood(good + 1)
+    const handleNeutralClick = () => setNeutral(neutral + 1)
+    const handleBadClick = () => setBad(bad + 1)
 
     return (
         <div>
-            <h1>Give Feedback</h1>
-            <button onClick={() => setGood(good + 1)}>good</button>
-            <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-            <button onClick={() => setBad(bad + 1)}>bad</button>
-            <h1>Statistics</h1>
-            <p>
-                good: {good} < br/>
-                neutral: {neutral} < br/>
-                bad: {bad} < br/>
-                all: {total} < br/>
-                average: {(good - bad) / total} < br/>
-                positive: {good / total * 100} % < br/>
-            </p>
+            <Header text='Give Feedback'/>
+            <Button handleClick={handleGoodClick} text='Good' />
+            <Button handleClick={handleNeutralClick} text='Neutral' />
+            <Button handleClick={handleBadClick} text='Bad' />
+            <Statistics good={good} neutral={neutral} bad={bad}/>
         </div>
     )
 }
---
+++ Part1/unicafe/src/components/Statistics.jsx
@@ -0,0 +1,30 @@
+﻿const Statistics = ({good, neutral, bad}) => {
+    const total = good + neutral + bad
+    if (total === 0) {
+        return <div>No feedback given</div>
+    }
+    const average = () => (good - bad) / total
+    const positive = () => (good / total) * 100 + '%'
+    return (
+        <table>
+            <tbody>
+            <StatisticLine text='good' value={good} />
+            <StatisticLine text='neutral' value={neutral} />
+            <StatisticLine text='bad' value={bad} />
+            <StatisticLine text='total' value={total} />
+            <StatisticLine text='average' value={average()} />
+            <StatisticLine text='positive' value={positive()} />
+            </tbody>
+        </table>
+    )}
+
+const StatisticLine = ({text, value}) => {
+    return (
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
+    )
+}
+
+export default Statistics
