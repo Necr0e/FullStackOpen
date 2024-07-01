ec19d75041716233efa72774236a0150cf8376fe
Necro- <necrotto@gmail.com>
2024-07-01 12:37:23 +0300

    Task 2.6-2.7 Start of the Phonebook app.

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas' }
+    ])
+    const [newName, setNewName] = useState('')
+
+    const addPerson = (event) => {
+        event.preventDefault()
+        const personObject = {
+            name: newName
+        }
+        const checkIfNameExists = persons.map(person => person.name)
+        if (checkIfNameExists.includes(newName)) {
+            alert(`${newName} is already added to the phonebook.`)
+            return
+        }
+        setPersons(persons.concat(personObject))
+        setNewName('')
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name:
+                    <input value={newName}
+                           onChange={handleNameChange}/>
+                </div>
+                <div>
+                    <button type='submit'>add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            <div>
+                {persons.map(person => <p key={person.name}>{person.name}</p>)}
+            </div>
+        </div>
+    )
+}
+
+export default App
